b03f69d8efa049655f9c3dea450d28f797459458
wolverine2714 <m202271789@hust.edu.cn>
2023-08-24 22:59:59 +0800

    fix: gitignore

--- src/main/java/com/hust/addrgeneration/serviceImpl/IPv6AddrServiceImpl.java
+++ src/main/java/com/hust/addrgeneration/serviceImpl/IPv6AddrServiceImpl.java
@@ -39,6 +39,7 @@ public class IPv6AddrServiceImpl implements IPv6AddrService {
         String phoneNumber = user.getPhoneNumber();
         String userName = user.getUsername();
 
+
         // step0. Check phoneNumber validation
         String phoneRegexp = "^((13[0-9])|(14[57])|(15[0-35-9])|(16[2567])|(17[0-8])|(18[0-9])|(19[0-9]))\\d{8}$";
         if(!Pattern.matches(phoneRegexp,phoneNumber)){
